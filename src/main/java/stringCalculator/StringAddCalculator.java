618cf96df925756f49e2f9f53c5103d6479810a2
juna.k <juna.k@navercorp.com>
2023-04-23 22:36:22 +0900

    feat: input 예외처리

--- src/main/java/stringCalculator/StringAddCalculator.java
+++ src/main/java/stringCalculator/StringAddCalculator.java
@@ -11,6 +11,9 @@ public class StringAddCalculator {
 	private static final String DEFAULT_DELIMITER = ":|,";
 
 	public static int splitAndSum(String input) {
+		if (input == null || input.length() == 0) {
+			return 0;
+		}
 
 		String delimiter = DEFAULT_DELIMITER;
 		Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
