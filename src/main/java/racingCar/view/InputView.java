9dc218580972279977d436c63d48188da41434df
juna.k <juna.k@navercorp.com>
2023-04-29 14:48:59 +0900

    feat: 자동차 이름을 입력받는다.

--- src/main/java/racingCar/view/InputView.java
+++ src/main/java/racingCar/view/InputView.java
@@ -1,12 +1,26 @@
 package racingCar.view;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class InputView {
+	private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 	private static final String DELIMITER = ",";
-	public static List<String> splitNames(String name) {
+
+	public static void printStartGameMessage() {
+		System.out.println(NAME_INPUT_MESSAGE);
+	}
+
+	public static List<String> getCarNames() {
+		return splitNames(getInput());
+	}
+
+	private static String getInput() {
+		Scanner scanner = new Scanner(System.in);
+		return scanner.next();
+	}
+
+	static List<String> splitNames(String name) {
 		return List.of(name.split(DELIMITER));
 	}
 }
